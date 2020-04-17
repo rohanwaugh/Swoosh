d77bef17ca19f727bd6fc134cfc7b2fdc3d33897
Rohan Wagh <rohan.wagh@cognizant.com>
2020-04-17 11:05:31 +0530

    Select League screen design implemented

---
+++ app/src/main/java/com/android/swoosh/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package com.android.swoosh
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LeagueActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
