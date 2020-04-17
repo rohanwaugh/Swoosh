a3fa490b98c4a0faa9eb934731f2c7798ed65f45
Rohan Wagh <rohan.wagh@cognizant.com>
2020-04-17 20:03:09 +0530

    Select Skill design implemented

--- app/src/main/java/com/android/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/android/swoosh/LeagueActivity.kt
@@ -1,11 +1,19 @@
 package com.android.swoosh
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+
 
 class LeagueActivity : BaseActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun launchSkillActivity(view: View) {
+        val skillIntent = Intent(this,SkillActivity::class.java)
+        startActivity(skillIntent)
     }
 }
