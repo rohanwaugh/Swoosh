10f5b152219df6d444200ae83979d9fa77f2ebff
Rohan Wagh <rohan.wagh@cognizant.com>
2020-04-17 12:28:59 +0530

    Navigation from welcome screen to league screen implemented

--- app/src/main/java/com/android/swoosh/MainActivity.kt
+++ app/src/main/java/com/android/swoosh/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.android.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton.setOnClickListener {
+            val leagueIntent = Intent(this,LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
+
 }
